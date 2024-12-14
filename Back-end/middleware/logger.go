d0731d3be624c4c99ccd81a942a157fd9aabca2f
Yasser <yaserqamhiah@gmail.com>
2024-12-14 13:30:23 +0300

    added Logger

--- Back-end/middleware/logger.go
+++ Back-end/middleware/logger.go
@@ -9,18 +9,16 @@ import (
 
 type ctxKeyLogger struct{}
 
-
-
-
 func LoggingMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger:=utils.GetLogger()
-        newCtx := context.WithValue(r.Context(), ctxKeyLogger{}, logger)
-        next.ServeHTTP(w, r.WithContext(newCtx))
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := utils.GetLogger()
+		newCtx := context.WithValue(r.Context(), ctxKeyLogger{}, logger)
+		next.ServeHTTP(w, r.WithContext(newCtx))
+	})
+
 }
 
-func FromContext(ctx context.Context) (*utils.Logger) {
-    logger := ctx.Value(ctxKeyLogger{}).(*utils.Logger)
-    return logger
+func FromContext(ctx context.Context) *utils.Logger {
+	logger := ctx.Value(ctxKeyLogger{}).(*utils.Logger)
+	return logger
 }
